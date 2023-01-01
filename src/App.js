0ac2755f2fdefb81c7d8c6c8aceeaf336df43d1c
Mohsen9april <Mohsen9april@gmail.com>
2023-01-02 02:13:40 +0330

    add footer structrue

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ColorLensRoundedIcon from "@mui/icons-material/ColorLensRounded";
 import SettingsRemoteRoundedIcon from "@mui/icons-material/SettingsRemoteRounded";
 import { motion } from "framer-motion";
 import { useState } from "react";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [open, setOpen] = useState(true);
@@ -57,97 +58,81 @@ function App() {
 
   return (
     <div className="App">
-      <div className="body_container">
-        <h2>Body</h2>
-        <hr />
-        <h2>Body</h2>
-        <hr />
-        <h2>Body</h2>
-        <hr />
-        <h2>Body</h2>
-        <hr />
-        <h2>Body</h2>
-        <hr />
-        <h2>Body</h2>
-        <hr />
-        <h2>Body</h2>
-        <hr />
-        <h2>Body</h2>
-        <hr />
-        <h2>Body</h2>
-        <hr />
-        <h2>Body</h2>
-        <hr />
-        <h2>Body</h2>
-        <hr />
-        <h2>Body</h2>
-        <hr />
-      </div>
-      <motion.div
-        data-Open={open}
-        variants={sideContainerVariants}
-        initial={`${open}`}
-        animate={`${open}`}
-        className="sidebar_container"
-      >
-        {/* sidebar */}
+      <div className="up_container">
+        {/* Body Container */}
+        <div className="body_container">
+          <h1>Body</h1>
+        </div>
+        {/* Sidebar Container */}
         <motion.div
+          data-Open={open}
+          variants={sideContainerVariants}
           initial={`${open}`}
           animate={`${open}`}
-          variants={sidebarVariants}
-          className="sidebar"
+          className="sidebar_container"
         >
-          {/* lines_icon */}
+          {/* sidebar */}
           <motion.div
-            whileHover={{
-              scale: 1.2,
-              rotate: 180,
-              backgroundColor: "rgba(255, 255, 255, 0.7)",
-              backdropFilter: "blur(4.5px)",
-              WebkitBackdropFilter: "blur(4.5px)",
-              border: "1px solid rgba( 255, 255, 255, 0.7 )",
-              transition: {
-                delay: 0.1,
-                duration: 0.25,
-              },
-            }}
-            onClick={handleToggle}
-            className="lines_icon"
-          >
-            <TocRoundedIcon />
-          </motion.div>
-          {/* profile */}
-          <motion.div
-            layout
             initial={`${open}`}
             animate={`${open}`}
-            variants={profileVariants}
-            transition={{ duration: 0.5 }}
-            whileHover={{
-              backgroundColor: "rgba(255, 255, 255, 0.3)",
-              boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
-              backdropFilter: "blur(5.5px)",
-              WebkitBackdropFilter: "blur(5.5px)",
-              border: "1px solid rgba( 255, 255, 255, 0.18 )",
-              cursor: "pointer",
-            }}
-            className="profile"
+            variants={sidebarVariants}
+            className="sidebar"
           >
-            <img src="./images/Logo.png" alt="profile_img" />
-          </motion.div>
-          <div className="groups">
-            <div className="group">
-              <ItemSidebar icon={<DashboardRoundedIcon />} name="صفحه اصلی" />
-              <ItemSidebar icon={<BarChartRoundedIcon />} name="محصولات" />
-              <ItemSidebar icon={<AttachMoneyRoundedIcon />} name="چارت سازمانی" />
-              <ItemSidebar icon={<AssignmentTurnedInRoundedIcon />} name="درباره ما" />
-              <ItemSidebar icon={<AccountCircleRoundedIcon />} name="تماس با ما" />
-              <ItemSidebar icon={<SettingsRemoteRoundedIcon />} name="گالری" />
-              <ItemSidebar icon={<ColorLensRoundedIcon />} name="پروژه خط جدید" />
+            {/* lines_icon */}
+            <motion.div
+              whileHover={{
+                scale: 1.2,
+                rotate: 180,
+                backgroundColor: "rgba(255, 255, 255, 0.7)",
+                backdropFilter: "blur(4.5px)",
+                WebkitBackdropFilter: "blur(4.5px)",
+                border: "1px solid rgba( 255, 255, 255, 0.7 )",
+                transition: {
+                  delay: 0.1,
+                  duration: 0.25,
+                },
+              }}
+              onClick={handleToggle}
+              className="lines_icon"
+            >
+              <TocRoundedIcon />
+            </motion.div>
+            {/* profile */}
+            <motion.div
+              layout
+              initial={`${open}`}
+              animate={`${open}`}
+              variants={profileVariants}
+              transition={{ duration: 0.5 }}
+              whileHover={{
+                backgroundColor: "rgba(255, 255, 255, 0.3)",
+                boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+                backdropFilter: "blur(5.5px)",
+                WebkitBackdropFilter: "blur(5.5px)",
+                border: "1px solid rgba( 255, 255, 255, 0.18 )",
+                cursor: "pointer",
+              }}
+              className="profile"
+            >
+              <img src="./images/Logo.png" alt="profile_img" />
+            </motion.div>
+            <div className="groups">
+              <div className="group">
+                <ItemSidebar icon={<DashboardRoundedIcon />} name="صفحه اصلی" />
+                <ItemSidebar icon={<BarChartRoundedIcon />} name="محصولات" />
+                <ItemSidebar icon={<AttachMoneyRoundedIcon />} name="چارت سازمانی" />
+                <ItemSidebar icon={<AssignmentTurnedInRoundedIcon />} name="درباره ما" />
+                <ItemSidebar icon={<AccountCircleRoundedIcon />} name="تماس با ما" />
+                <ItemSidebar icon={<SettingsRemoteRoundedIcon />} name="گالری" />
+                <ItemSidebar icon={<ColorLensRoundedIcon />} name="پروژه خط جدید" />
+              </div>
             </div>
-          </div>
+          </motion.div>
         </motion.div>
-      </motion.div>
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Footer/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer_container">
+      <h1>Footer</h1>
+      <p>Copyright © 2022 Mehraeinpars All rights reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
