c9b88cfea1ad9f262a18d56e8c45e28610e6f506
Mohsen9april <Mohsen9april@gmail.com>
2023-01-10 23:50:48 +0330

    add timeline newline content

--- src/components/Pages/Newline.js
+++ src/components/Pages/Newline.js
@@ -1,39 +1,45 @@
 import React from "react";
+import { ReactComponent as WorkIcon } from "../../../src/work.svg";
+import { ReactComponent as SchoolIcon } from "../../../src/school.svg";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { FaTimes, FaBars } from "react-icons/fa";
+import timelineElements from "./timelineElements";
 
 const Newline = () => {
+  let workIconStyles = { background: "#06D6A0" };
+  let schoolIconStyles = { background: "#f9c74f" };
+
   return (
-    <div className="App">
-      <h2>Timeline </h2>
-      <VerticalTimeline lineColor="#4f0601" animate={true}>
-        {/* 1 */}
-        <VerticalTimelineElement
-          className="vertical-timeline-element"
-          date="2010 - 2014"
-          iconStyle={{ background: "#3e497a", color: "#fff" }}
-          icon={<FaBars />}
-        >
-          <h2>Section 1</h2>
-          <h4 className="messages">
-            Bachelor's Degree Bachelor's Degree Bachelor's Degree Bachelor's Degree Bachelor's
-            Degree
-          </h4>
-        </VerticalTimelineElement>
-        {/* 2 */}
-        <VerticalTimelineElement
-          className="vertical-timeline-element"
-          date="2014 - 2023"
-          iconStyle={{ background: "#88497a", color: "#fff" }}
-          icon={<FaTimes />}
-        >
-          <h2>Section 2</h2>
-          <h4 className="messages">
-            Bachelor's Degree Bachelor's Degree Bachelor's Degree Bachelor's Degree Bachelor's
-            Degree
-          </h4>
-        </VerticalTimelineElement>
+    <div>
+      <h1 className="title">Timeline</h1>
+      <VerticalTimeline>
+        {timelineElements.map((element) => {
+          let isWorkIcon = element.icon === "work";
+          let showButton =
+            element.buttonText !== undefined &&
+            element.buttonText !== null &&
+            element.buttonText !== "";
+
+          return (
+            <VerticalTimelineElement
+              key={element.key}
+              date={element.date}
+              dateClassName="date"
+              iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+              icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+            >
+              <h3 className="vertical-timeline-element-title">{element.title}</h3>
+              <h5 className="vertical-timeline-element-subtitle">{element.location}</h5>
+              <p id="description">{element.description}</p>
+              {showButton && (
+                <a className={`button ${isWorkIcon ? "workButton" : "schoolButton"}`} href="/">
+                  {element.buttonText}
+                </a>
+              )}
+            </VerticalTimelineElement>
+          );
+        })}
       </VerticalTimeline>
     </div>
   );
